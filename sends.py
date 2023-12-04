43c73b5f86b2bfd701b11f2a14db2a5354dee980
djumanov <diyorbekjumanov1120@gmail.com>
2023-12-04 10:49:24 +0500

    init

---
+++ sends.py
@@ -0,0 +1,29 @@
+def send_message(chat_id: int, text, parse_mode=False):
+    pass
+
+def send_contact(chat_id: int, text, parse_mode=False):
+    pass
+
+def send_video(chat_id: int, text, parse_mode=False):
+    pass
+
+def send_location(chat_id: int, text, parse_mode=False):
+    pass
+
+def send_message(chat_id: int, text, parse_mode=False):
+    pass
+
+def send_message(chat_id: int, text, parse_mode=False):
+    pass
+
+def send_message(chat_id: int, text, parse_mode=False):
+    pass
+
+def send_message(chat_id: int, text, parse_mode=False):
+    pass
+
+def send_message(chat_id: int, text, parse_mode=False):
+    pass
+
+def send_message(chat_id: int, text, parse_mode=False):
+    pass
