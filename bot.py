32dcc2cc58623daaef5fbc13bb9e976bfb1db868
musayevjahongir <musayevjahongir5051@gmail.com>
2023-12-05 08:28:20 +0500

    echo bot

---
+++ bot.py
@@ -0,0 +1,93 @@
+from settings import TOKEN
+import requests
+from time import sleep
+from pprint import pprint
+import sends
+
+
+def get_updates():
+    url = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()['result']
+    else:
+        return False
+
+
+def get_last_update(updates: list[dict]) -> dict:
+    return updates[-1]
+
+# pprint(get_updates())
+
+last_update_id = -1
+while True:
+    updates = get_updates()
+    # print(updates)
+    last_update = get_last_update(updates)
+    if last_update['update_id'] != last_update_id:
+            user = last_update['message']['from']
+            if "photo" in last_update['message'].keys():
+                photo=last_update['message']["photo"][0]["file_id"]
+
+                sends.send_photo(user["id"], photo)
+            
+            elif "text" in last_update['message'].keys():
+
+                text = last_update['message']['text']
+                sends.send_message(user["id"], text)
+
+            elif "contact" in last_update['message'].keys():
+                 chat_id=user["id"]
+                 number=last_update['message']["contact"]["phone_number"]
+                 first_name=last_update['message']["contact"]["first_name"]
+                 if "last_name" in last_update["message"]['contact'].keys():
+                    last_name=last_update['message']["contact"]["last_name"]
+                    sends.send_contact(chat_id, number, first_name, last_name)
+                 else:
+                     sends.send_contact(chat_id, number, first_name)
+                
+
+            elif "location" in last_update["message"].keys():
+
+                latitude=last_update["message"]["location"]["latitude"]                
+                longitude=last_update["message"]["location"]["longitude"]   
+                sends.send_location(user["id"], latitude, longitude)   
+
+            elif "video" in last_update["message"].keys():
+
+                video=last_update["message"]["video"]["file_id"]
+
+                sends.send_video(user["id"], video)
+            elif "document" in last_update["message"].keys():
+
+                document=last_update["message"]["document"]["file_id"]
+
+                sends.send_video(user["id"], document)
+
+            elif "voice" in last_update["message"].keys():
+
+                voice=last_update["message"]["voice"]["file_id"]
+
+                sends.send_video(user["id"], voice)
+            
+            elif "audio" in last_update["message"].keys():
+
+                audio=last_update["message"]["audio"]["file_id"]
+
+                sends.send_video(user["id"], audio)
+
+            elif "video_note" in last_update["message"].keys():
+
+                video_note=last_update["message"]["video_note"]["file_id"]
+
+                sends.send_video_note(user["id"], video_note)
+
+            elif "emoji" in last_update["message"].keys():
+
+                emoji=last_update["message"]["emoji"]
+
+                sends.send_video_note(user["id"], emoji)
+
+            last_update_id = last_update['update_id']
+    sleep(0.5)
--- sends.py
+++ sends.py
@@ -1,29 +1,114 @@
-def send_message(chat_id: int, text, parse_mode=False):
-    pass
+from settings import TOKEN, URL
+import requests
 
-def send_contact(chat_id: int, text, parse_mode=False):
-    pass
-
-def send_video(chat_id: int, text, parse_mode=False):
-    pass
-
-def send_location(chat_id: int, text, parse_mode=False):
-    pass
 
 def send_message(chat_id: int, text, parse_mode=False):
-    pass
+    url = URL+'sendMessage'
+    payload = {
+        "chat_id": chat_id,
+        "text": text,
+    }
+    if parse_mode:
+        payload["parse_mode"] = "HTML"
 
-def send_message(chat_id: int, text, parse_mode=False):
-    pass
+    requests.get(url, params=payload)
 
-def send_message(chat_id: int, text, parse_mode=False):
-    pass
+def send_contact(chat_id, numer, fname, lname=None):
+    url = URL+"sendContact"
+    payload = {
+        "chat_id": chat_id,
+        "phone_number": numer,
+        "first_name": fname,
+    }
+    if lname:
+        payload['last_name'] = lname
 
-def send_message(chat_id: int, text, parse_mode=False):
-    pass
+    requests.post(url, params=payload)
 
-def send_message(chat_id: int, text, parse_mode=False):
-    pass
+def send_photo(chat_id, photo):
+    url=URL+'sendPhoto'
+    payload = {
+        "chat_id": chat_id,
+        "photo": photo,
+    }
+    requests.get(url, params=payload)
 
-def send_message(chat_id: int, text, parse_mode=False):
-    pass
+def send_location(chat_id: int, latitude, longitude):
+    url=URL+"sendLocation"
+
+    payload = {
+        "chat_id" : chat_id,
+        "latitude" : latitude,
+        "longitude" : longitude
+    }
+
+    requests.get(url, params = payload)
+
+def send_video(chat_id: str, video):
+    url=URL+"sendVideo"
+
+    payload = {
+        "chat_id" : chat_id,
+        "video" : video
+    }
+
+    requests.get(url, params=payload)
+
+def send_document(chat_id: str, document ):
+    url=URL+"sendDocument"
+
+    payload = {
+        "chat_id" : chat_id,
+        "document" : document
+    }
+
+    requests.get(url, params=payload)
+
+def send_voice(chat_id: int, voice):
+    url=URL+"sendVoice"
+
+    payload = {
+        "chat_id" : chat_id,
+        "voice" : voice
+    }
+
+    requests.get(url, params=payload)
+
+def send_audio(chat_id: int, audio):
+    url=URL+"sendAudio"
+
+    payload = {
+        "chat_id" : chat_id,
+        "audio" : audio
+    }
+
+    requests.get(url, params=payload)
+
+def send_video_note(chat_id: int, video_note):
+    url=URL+"sendVideoNote"
+
+    payload = {
+        "chat_id" : chat_id,
+        "video_note" : video_note
+    }
+
+    requests.get(url, params=payload)
+
+def send_emoji(chat_id, emoji=None):
+    url=URL+"sendDice"
+
+    payload = {
+        "chat_id" : chat_id,
+        "emoji" : emoji
+    }
+
+    requests.get(url, params=payload)
+
+
+# send_contact("927181585", "+998990235051", "Jahongir", "Musayev")
+# send_location("927181585", "39.658174", "66.919475")
+# send_video("927181585", "BAACAgIAAxkBAANIZW3azxfWc5PKL4v5plqYswTroI8AAqg7AAKQQ3BLKNmZvYXQjlgzBA")
+# send_document("927181585", "BQACAgIAAxkBAANKZW3bz4mcGMzN8gupN-uVlrBdOOgAArg7AAKQQ3BLcr3vFs2gTuozBA")
+# send_voice("927181585","AwACAgIAAxkBAANMZW3dcQgPQZfXNavE2Xb_I-T8O1gAAsw7AAKQQ3BLOrWaKeOJFfszBA")
+# send_audio("927181585" , "CQACAgIAAxkBAANPZW3zjQeaRmuC8pF4jnNzzo-5lJkAAgM5AAKEc5FJLyRl4IhqbsozBA")
+send_emoji("927181585")
